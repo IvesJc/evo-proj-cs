4d050b0dcb311f142cd7f316dcab76e58225019b
Ives Chiba <ivesjundi@gmail.com>
2024-09-12 15:14:55 -0300

    feat :sparkles: func and dep interfaces

---
+++ EvoSystems/Services/Departamento/IDepartamentoInterface.cs
@@ -0,0 +1,14 @@
+﻿using EvoSystems.Controllers.Departamento.Dto;
+using EvoSystems.Models;
+
+namespace EvoSystems.Services.Departamento;
+
+public interface IDepartamentoInterface
+{
+    Task<List<DepartamentoResponseDto>> GetAllDepartamentos();
+    Task<DepartamentoResponseDto> CreateDepartamento(DepartamentoRequestDto departamentoRequestDto);
+    Task<DepartamentoResponseDto> UpdateDepartamento(int idDep, DepartamentoRequestDto departamentoRequestDto);
+    Task<DepartamentoResponseDto> DeleteDepartamento(int idDep);
+    
+
+}
---
+++ EvoSystems/Services/Funcionario/IFuncionarioInterface.cs
@@ -0,0 +1,11 @@
+﻿using EvoSystems.Controllers.Funcionario.Dto;
+
+namespace EvoSystems.Services.Funcionario;
+
+public interface IFuncionarioInterface
+{
+    Task<List<FuncionarioResonseDto>> GetAllFuncionarios();
+    Task<FuncionarioResonseDto> CreateFuncionario(FuncionarioRequestDto funcionarioRequestDto);
+    Task<FuncionarioResonseDto> UpdateFuncionario(int funcionarioId, FuncionarioRequestDto funcionarioRequestDto);
+    Task<FuncionarioResonseDto> DeleteFuncionario(int funcionarioId);
+}
