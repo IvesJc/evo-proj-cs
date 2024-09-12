bb7daa9c4586e8575fead9ce3402cc1ec52f979f
Ives Chiba <ivesjundi@gmail.com>
2024-09-12 15:14:38 -0300

    feat :sparkles: funcionario and departamento controller (TODO: implement service)

---
+++ EvoSystems/Controllers/Funcionario/FuncionarioController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvoSystems.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FuncionarioController : ControllerBase
+    {
+    }
+}
