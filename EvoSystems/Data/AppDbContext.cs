08c6ae672252023a1e76e9ba04600dac202cef37
Ives Chiba <ivesjundi@gmail.com>
2024-09-12 14:35:31 -0300

    feat :sparkles: DbContext

---
+++ EvoSystems/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EvoSystems.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EvoSystems.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Departamento> Departamentos { get; set; }
+    public DbSet<Funcionario> Funcionarios { get; set; }
+    
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
